ced5d3f6dba7574285f03d19d32a54c161885ce1
Marcelo Basso <bassomarcelo.ba@gmail.com>
2024-07-20 22:54:39 -0300

    ✨ adds hashtable statistics

--- lab4-hashtable/hashtable.cpp
+++ lab4-hashtable/hashtable.cpp
@@ -2,50 +2,110 @@
 
 using namespace std;
 
-bool isPrime(int num) {
-    if (num <= 1 || num % 2 == 0) return false;
-    if (num == 2) return true;
-
-    for (int i = 3; i <= sqrt(num); i += 2) {
-        if (num % i == 0) return false;
-    }
-
-    return true;
-}
-
-int nextPrime(int n) {
-    while (!isPrime(n)) n++;
-
-    return n;
-}
-
-int calculate_hash(int id, int M) {
-    return id % nextPrime(floor(M * 1.2));
-}
-
-
-
-int get_nlines(string file) {
-    int number_of_lines = 0;
-    string line;
+vector<vector<Player>> create_hashtable(string file, int sizeHash, ofstream &output) {
+    vector<vector<Player>> hashtable(sizeHash);
+    string temp;
     ifstream input(file);
+    auto start = chrono::high_resolution_clock::now();
+    int occupied = 0, list_size, max_list = 0, entries = 0;
 
-    while (getline(input, line))
-        ++number_of_lines;
+    // ignores the header of the file
+    getline(input, temp);
+    while (getline(input, temp)) {
+        list_size = insertHash(temp, hashtable, sizeHash, &occupied);
+        if (list_size > max_list) max_list = list_size;
+        entries++;
+    }
+    
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    output << "PARTE1: ESTATISTICAS DA TABELA HASH" << endl;
+    output << "TEMPO DE CONSTRUCAO DA TABELA " << duration << "ms" << endl;
+    output << "TAXA DE OCUPACAO " << (float) occupied/sizeHash << endl;
+    cout << occupied << " " << sizeHash << endl;
+    output << "TAMANHO MAXIMO DE LISTA " << max_list << endl;
+    output << "TAMANHO MEDIO DE LISTA " << entries/occupied << endl;
     
     input.close();
-    return number_of_lines;
+    return hashtable;
 }
 
-// vector<vector<pair<int, Player>>>
-void create_hashtable(string file) {
-    int nlines=get_nlines(file);
-    int s_vector=nextPrime(floor(nlines * 1.2));
-    vector<vector<pair<int, Player>>> hashtable(s_vector);
-    ifstream input (file);
+int insertHash(string stats_player, vector<vector<Player>> &hashtable, int sizeHash, int *occupied) {
+    stringstream ss(stats_player);
     string temp;
-    while(input>>temp){
-        
+    Player auxPlayer;
+    vector<string> dados;
+    int key;
+
+    for (int i=0; i<2;i++) {
+        getline(ss, temp, ',');
+        dados.push_back(temp);
     }
+    getline(ss, temp);
+    dados.push_back(temp);
+    auxPlayer.sofifa_id = stoi(dados[0]);
+    auxPlayer.name = dados[1];
+    auxPlayer.positions = dados[2];
+    key = auxPlayer.sofifa_id % sizeHash;
+
+    //cout << hashtable[key].size(); 
+    if (hashtable[key].empty()) (*occupied)++;
+    hashtable[key].push_back(auxPlayer);
+
+    return hashtable[key].size();
+}
+
+Player searchHash(int fifa_id, vector<vector<Player>> &hashtable, int &tests, int &found) {
+    int i = 0;
+    int key = fifa_id % hashtable.size();
+    Player auxPlayer;
+
+    auxPlayer.sofifa_id=fifa_id;
+    auxPlayer.name="NAO ENCONTRADO";
+    auxPlayer.positions="";
+
+    if(!hashtable[key].empty()){
+        while ((i < hashtable[key].size()) && hashtable[key][i].sofifa_id != fifa_id) i++;
+        if (hashtable[key][i].sofifa_id == fifa_id) {
+            tests+=i;
+            found++;
+            auxPlayer.name=hashtable[key][i].name;
+            auxPlayer.positions=hashtable[key][i].positions;
+        }
+    }
+
+    return auxPlayer;
+}
+
+void searchHashArq(vector<vector<Player>> &hashtable, ofstream &output) {
+    ifstream input("input/consultas.csv");
+    string temp;
+    Player auxPlayer;
+    vector<Player> consultas;
+    int n_test = 0, deltaTest = 0, maxTest = 0, found = 0;
+
+    output << "PARTE 2: ESTATISTICAS DAS CONSULTAS" << endl;
+    auto start = chrono::high_resolution_clock::now();
+
+    while (getline(input, temp)) {
+        deltaTest = n_test;
+        consultas.push_back(searchHash(stoi(temp), hashtable, n_test, found));
+        deltaTest = n_test - deltaTest;
+        if (deltaTest >= maxTest) maxTest = deltaTest;
+    }
+
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    output << "TEMPO DE REALIZACAO DE TODAS AS CONSULTAS: " << duration << "ms"<< endl;
+
+    for (int i=0; i<consultas.size(); i++) {
+        output << consultas[i].sofifa_id << " " << consultas[i].name << endl;
+    }
+
+    output << "MAXIMO DE NUMERO DE TESTES POR NOME ENCONTRADO: " << maxTest << endl;
+    output << "MEDIA NUMERO DE TESTES POR NOME ENCONTRADO: " << n_test/found << endl;
+
+    input.close();
+    output.close();
 }
 
--- lab4-hashtable/main.cpp
+++ lab4-hashtable/main.cpp
@@ -1,5 +1,20 @@
 #include "hashtable.h"
 
 int main() {
-    create_hashtable("input/players.csv");
+    vector<vector<Player>> hashtable;
+    vector<string> exp = {"experimento997.txt", "experimento1999.txt", "experimento3989.txt", "experimento7993.txt"};
+    ifstream input_f;
+
+    int sizeHash[4] = {997, 1999, 3989, 7993};
+    for (int i = 0; i < 4; i++) {
+        ofstream output_f(exp[i]);
+        cout << "Creating HashTable with size " << sizeHash[i] << "..."  << endl;
+        hashtable = create_hashtable("input/players.csv", sizeHash[i], output_f);
+        cout << "Searching..." << endl;
+        searchHashArq(hashtable, output_f);
+
+        output_f.close();
+    }
+
+    return 0;
 }
